d2231a213a465647bad4be5d3b9f613ccd28e3aa
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-10-28 11:52:13 -0500

    agregar a division , loops, arithemetic operadores

---
+++ arithmetic_operators.py
@@ -0,0 +1,16 @@
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def multiplicacion(a,b):
+    return a*b
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(suma(a,b))
+    print(resta(a,b))
+    print (multiplicacion(a,b))
---
+++ division.py
@@ -0,0 +1,13 @@
+def division():
+    a = int(input())
+    b = int(input())
+    c = int(a /b)
+    d = float(a/b) 
+    print(c)
+    print(d)
+
+
+    
+
+if __name__ == '__main__':
+    division()
