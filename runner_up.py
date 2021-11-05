281da34ef5ea18cf63ddc3b2740dead9f1aced61
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-11-05 14:03:32 -0600

    aun no queda den ordenamiento

--- runner_up.py
+++ runner_up.py
@@ -1,9 +1,10 @@
 def main():
+    n = int(input())
     numeros = list(map(int,input().split()))
     #print(numeros)
     lista = ordanamiento(numeros)
-    print(lista)
-    segundo = lista[1]
+    #print(lista)
+    segundo = str(lista[1])
     print(segundo)
     
 
@@ -14,7 +15,7 @@ def ordanamiento(our_list):
 
     for i in range(len(our_list)):
         # We want the last pair of adjacent elements to be (n-2, n-1)
-        for j in range(len(our_list) - 1):
+        for j in range(len(our_list)-1):
             if our_list[j] < our_list[j+1]:
                 # Swap
                 our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
