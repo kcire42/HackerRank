54c3278da23b3a543fcf3ecd7fd477b5f2d60547
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-11-04 23:36:18 -0600

    se agrega el buble sort que aun no entiendo chale

---
+++ loop.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input("Ingrese un numero: "))
+    for i in range(n):
+        elevado= i**2
+        print(elevado)
+
+
+
+if __name__ == '__main__':
+    main()
--- runner_up.py
+++ runner_up.py
@@ -1,10 +1,24 @@
 def main():
     numeros = list(map(int,input().split()))
+    #print(numeros)
+    lista = ordanamiento(numeros)
+    print(lista)
+    segundo = lista[1]
+    print(segundo)
     
 
     
-    print ()
+    
 
+def ordanamiento(our_list):
+
+    for i in range(len(our_list)):
+        # We want the last pair of adjacent elements to be (n-2, n-1)
+        for j in range(len(our_list) - 1):
+            if our_list[j] < our_list[j+1]:
+                # Swap
+                our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
+    return our_list
 
 if __name__ == '__main__':
     main()
