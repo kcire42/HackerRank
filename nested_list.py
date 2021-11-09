b69d0ad6c42486404f7e5316277f293cb9329073
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-11-09 14:43:31 -0600

    Se agrega el modo de bubble y delimitacion de empates para nested list

--- nested_list.py
+++ nested_list.py
@@ -4,16 +4,28 @@ def main():
         name = input()
         score = float(input())
         lista.append([name,score])
-    print(lista)
+    #print(lista)
+    #algoritmo para ordenar los datos de menor a mayor 
     for i in range(len(lista)):
         # print(lista[i])
         # print(lista[i][1])
         for j in range(len(lista)-1):
             if lista[j][1] > lista[j+1][1]:
-                lista[j][1] , lista[j+1][1] = lista[j+1][1] , lista[j][1]
-                
-    
+                lista[j] , lista[j+1] = lista[j+1], lista[j]
+            
     print(lista)
+    #dejar solo el segundo lugar
+    segundo_lugar = [lista[1]]
+    #print(segundo_lugar[0][1])
+    #algoritmo para revisar si hay empates en alguna posicion
+    for participante in range(len(lista[2::])):
+        #print(lista[participante+2][1])
+        if segundo_lugar[0][1] == lista[participante+2][1]:
+           segundo_lugar.append(lista[participante+2])
+       
+
+        
+    print(segundo_lugar)
 
 
 if __name__ == '__main__':
