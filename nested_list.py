85c35d7aa7862f43357e715f3c85b15d0be677ed
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-11-11 10:56:49 -0600

    se termina la listas anidabas y se crea archivo para encontrar procentaje

--- nested_list.py
+++ nested_list.py
@@ -26,18 +26,24 @@ def main():
     segundo_lugar = []
     for participante in range(len(lista)):
         #print(lista[0][1])
-        if lista[0][1] < lista[participante][1] and lista[participante][1] < lista[participante+1][1]:
+        if lista[0][1] < lista[participante][1]:
             segundo_lugar.append(lista[participante])
+    segundo_lugar_correcto = [segundo_lugar[0]]
+    for participante_2 in range(len(segundo_lugar[1::])):
+        #print(lista[participante+2][1])
+        if segundo_lugar_correcto[0][1] == segundo_lugar[participante_2+1][1]:
+           segundo_lugar_correcto.append(segundo_lugar[participante_2+1])  
+    #print(segundo_lugar_correcto)
 
-    print(segundo_lugar)
-    # segundo_lugar.sort()
-    # for nombres in range(len(segundo_lugar)):
-    #     print(segundo_lugar[nombres][0])
+    #print(segundo_lugar)
+    segundo_lugar_correcto.sort()
+    for nombres in range(len(segundo_lugar_correcto)):
+        print(segundo_lugar_correcto[nombres][0])
 
 
 if __name__ == '__main__':
     main()
-#Harsh
+# Harsh
 # 20
 # Beria
 # 20
