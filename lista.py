c77626d59ffb138786b4b08c3904a82a1dc0f983
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-11-19 00:06:39 -0600

    inicio lista aun no queda

--- lista.py
+++ lista.py
@@ -3,7 +3,33 @@ def main():
     lista = []
     for instruccion in range(numero_instrucciones):
         instruccion_a_ejecutar = list(input().split())
-        print(instruccion_a_ejecutar)
+        cmd = instruccion_a_ejecutar[0]
+
+        if cmd == "insert":
+            #[1]posicion [2]objeto
+            posicion = int(instruccion_a_ejecutar[1])
+            objeto = int(instruccion_a_ejecutar[2])
+            lista.insert(posicion,objeto)
+            
+
+            
+        elif cmd == "print":
+            print(lista)
+        elif cmd == "remove":
+            objeto = int(instruccion_a_ejecutar[1])
+            lista.remove(objeto)
+        elif cmd == "append":
+            objeto = int(instruccion_a_ejecutar[1])
+            lista.append(objeto)
+        elif cmd == "sort":
+            lista.sort
+        elif cmd == "pop":
+            
+            lista.pop
+        elif cmd == "reverse":
+            
+            lista.reverse
+        
 
 
 if __name__=='__main__':
