520e898c36d8b02d8dddf1399b283c005c674a7e
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-11-26 15:21:24 -0600

    Terminar tuplas

--- tuples.py
+++ tuples.py
@@ -1,6 +1,13 @@
 def main():
+    # n = int(input())
+    # # numeros = hash(tuple((map(int, input().split()))))
+    # # print(numeros)
+    # print(input() == 0 or hash(tuple([int(x) for x in (input().strip().split())])))
+    # # input()
+    # # print(hash(tuple(int(x) for x in input().split())))
     n = int(input())
-    numeros = list((map(int, input().split()))
+    integer_list = hash(tuple(map(int, input().split())))
+    print(integer_list)
     
     
 
