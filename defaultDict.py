72f36edd4701ec0007bbd5cbd23aca0cdf72df57
Erick Carrillo <erickcarrillo2807@gmail.com>
2022-02-18 08:42:42 -0600

    Ejercicios defaulDict y itertools_product

---
+++ defaultDict.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+# d = defaultdict(list)
+# list1=[]
+
+# n, m = map(int,raw_input().split())
+
+# for i in range(0,n):
+#     d[raw_input()].append(i+1) 
+
+# for i in range(0,m):
+#     list1=list1+[raw_input()]  
+
+# for i in list1: 
+#     if i in d:
+#         print " ".join( map(str,d[i]) )
+#     else:
+#         print -1
+
+
+def main():
+    d = defaultdict(list)
+    n, m = map(int, input().split())
+    lista = []
+    for i in range(0,n):
+        d[input()].append(i+1)
+    #print(d)
+    for j in range(0,m):
+        lista = lista + [input()]
+    #print(lista)
+    for k in lista:
+        if k in d:
+            print (" ".join(map(str,d)))
+        else:
+            print(-1)
+        
+        
+
+    
+
+
+if __name__ == "__main__":
+    main()
