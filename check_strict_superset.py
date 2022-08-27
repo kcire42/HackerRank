ee7d120dfb7066a357ecf6cca10d7087f89dcb09
Erick Carrillo <erickcarrillo2807@gmail.com>
2022-08-27 13:44:51 -0500

    super set

---
+++ check_strict_superset.py
@@ -0,0 +1,9 @@
+def check_strict_superset():
+    super_grupo_A = set(map(int,input().split()))
+    numero_grupo = int(input())
+    for grupo in range(numero_grupo):
+        grupo = 
+
+
+if __name__ == '__main__':
+    check_strict_superset()
