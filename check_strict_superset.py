df211dd89dd1205d19356e2d85d434f74208925d
Erick Carrillo <erickcarrillo2807@gmail.com>
2022-08-29 12:49:32 -0500

    check strick superset programa que verifique si tengo unca cadena en un superset

--- check_strict_superset.py
+++ check_strict_superset.py
@@ -1,8 +1,18 @@
 def check_strict_superset():
-    super_grupo_A = set(map(int,input().split()))
-    numero_grupo = int(input())
-    for grupo in range(numero_grupo):
-        grupo = 
+    super_group_A = set(map(int,input().split()))
+    numbers_of_group = int(input())
+    count = 0 
+    for group in range(numbers_of_group):
+        group = set(map(int,input().split()))
+        status_group = group.issubset(super_group_A)
+        if status_group == True:
+            count += 1
+
+    if count == numbers_of_group:
+        print("True")
+    else:
+        print("False")
+    
 
 
 if __name__ == '__main__':
